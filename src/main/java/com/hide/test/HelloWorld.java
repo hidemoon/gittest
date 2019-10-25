76921cebcfade38c31eadd03265bb4d99b0fce82
hide <18995502882@189.cn>
2019-10-25 17:15:26 +0800

    创建项目

---
+++ src/main/java/com/hide/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.hide.test;
+
+/**
+ * @Classname com.hide.test.HelloWorld
+ * @Description TODO
+ * @Date 19/10/25 17:12
+ * @Created by hideliu
+ */
+public class HelloWorld {
+    //第一个文件
+}
